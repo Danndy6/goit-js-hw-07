9865531a42967007e7f2cafa0b0df8508588339d
Danndy6 <danndy754@gmail.com>
2021-12-15 23:16:16 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -49,3 +49,59 @@ function onEscKeyPress(e) {
 window.addEventListener('click', onEscKeyPress);
 refs.gallery.addEventListener('click', onOpenModal);
 
+// const instance = basicLightbox.create(`
+//   <img src="" />
+// `);
+
+// const refs = {
+//     gallery: document.querySelector('.gallery'),
+//     modal: instance.element().querySelector('img'),
+// };
+
+// const galleryMarkup = createGalleryMarkup();
+// refs.gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+
+// function createGalleryMarkup() {
+//   return galleryItems
+//     .map(({ preview, original, description }) => {
+//       return `
+//     <div class="gallery__item">
+//   <a
+//     class="gallery__link"
+//     href="${original}"
+//   >
+//     <img
+//       class="gallery__image"
+//       src="${preview}"
+//       data-source="${original}"
+//       alt="${description}"
+//     />
+//   </a>
+// </div>
+//     `;
+//     })
+//     .join('');
+// };
+
+// refs.gallery.addEventListener('click', onOpenModal);
+
+// function onOpenModal(event) { 
+//  event.preventDefault() 
+//   if (event.target.nodeName !== 'IMG') {
+//     return;
+//     };
+//   refs.modal.src = event.target.dataset.source;
+//   instance.show()
+//   window.addEventListener('keydown',onEscKeyPress)
+// };
+
+// function onEscKeyPress(event) {
+//    event.preventDefault() 
+//   const isEscKey = event.keyCode === 27;
+//   console.log(event);
+//   if (isEscKey) {
+//     instance.close();
+//     window.removeEventListener('keydown', onEscKeyPress);
+//   refs.modal.src = ''
+//     };
+// };
